39265dd4888f67f9706931839543b4ac87095823
SilentStorm <carl.silence@gmail.com>
2019-10-31 21:43:47 +0100

    swap pleace public and private components

--- header/Window.hpp
+++ header/Window.hpp
@@ -5,11 +5,6 @@
 
 class Window
 {
-private:
-    const int width_;
-    const int height_;
-    const std::string name_;
-
 public:
     Window();
     ~Window();
@@ -17,6 +12,11 @@ public:
     int getWidth() const;
     int getHeight() const;
     std::string getName() const;
+    
+private:
+    const int width_;
+    const int height_;
+    const std::string name_;
 };
 
 #endif
